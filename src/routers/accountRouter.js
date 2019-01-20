fe6ba103c9ea2d2bcf5d692749660805df489951
Ganchinaifen <1352380806@qq.com>
2019-01-20 15:05:41 +0800

    01-干吃奶粉v1.0

---
+++ src/routers/accountRouter.js
@@ -0,0 +1,14 @@
+/**
+ * 注册和登录
+ */
+//导包
+const express = require('express')
+const path = require('path')
+//创建路由对象
+const accountRouter = express.Router()
+//导入控制器模块
+const accountController = require(path.join(__dirname, '../controllers/accountController'))
+//处理请求
+accountRouter.get('/register', accountController.getRegisterPage)
+//导出
+module.exports = accountRouter;
